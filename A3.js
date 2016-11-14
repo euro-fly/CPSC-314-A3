4e56ce608cb197393e9a790c4592b456d9209c18
jrm <haca.plus@gmail.com>
2016-11-14 11:04:47 -0800

    Revert "tweak cube width"
    This reverts commit 53efc38cb78f1fbe7c407d193bc72ac6fb9ac5e1.

--- A3.js
+++ A3.js
@@ -61,23 +61,23 @@ var baseColor = {type: "c", value: new THREE.Color(1.0,1.0,1.0)};
 
 
 //TODO: set up separate CubeCamera objects for the three reflective spheres!
-var cubeCamera = new THREE.CubeCamera(1,1000, sphereRadius * 2);
+var cubeCamera = new THREE.CubeCamera(1,1000, 256);
 cubeCamera.renderTarget.minFilter = THREE.LinearMipMapLinearFilter;
 cubeCamera.position.copy(camera.position);
 scene.add(cubeCamera);
 
-var gouraudCube = new THREE.CubeCamera(1, 1000, sphereRadius * 2.0);
+var gouraudCube = new THREE.CubeCamera(1, 1000, 256);
 gouraudCube.renderTarget.minFilter = THREE.LinearMipMapLinearFilter;
 gouraudCube.position.set(-7.5, sphereRadius, 0);
 scene.add(gouraudCube);
 
-var phongCube = new THREE.CubeCamera(1, 1000, sphereRadius * 2.0);
+var phongCube = new THREE.CubeCamera(1, 1000, 256);
 phongCube.renderTarget.minFilter = THREE.LinearMipMapLinearFilter;
 phongCube.position.set(-2.5, sphereRadius, 0);
 scene.add(phongCube);
 
 
-var blinnPhongCube = new THREE.CubeCamera(1, 1000, sphereRadius * 2.0);
+var blinnPhongCube = new THREE.CubeCamera(1, 1000, 256);
 blinnPhongCube.renderTarget.minFilter = THREE.LinearMipMapLinearFilter;
 blinnPhongCube.position.set(2.5, sphereRadius, 0);
 scene.add(blinnPhongCube);
